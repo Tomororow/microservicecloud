c315c3198746f5c49aafb63ef36f8cd5b551efa9
Tonight <Tonight@git.com>
2020-02-20 20:48:45 +0800

    springcloud addmoudle

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -0,0 +1,53 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DeptController {
+
+    @Autowired
+    private DeptService deptService;
+
+    @RequestMapping(value = "/dept/add",method = RequestMethod.POST)
+    public boolean add(Dept dept){
+        return deptService.add(dept);
+    }
+
+    //一旦调用服务方法失败并抛出了错误信息后，会自动调用@HystrixCommand标注好的fallbackMethod调用类中的指定方法
+    @RequestMapping(value = "/dept/get/{id}",method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
+    public Dept get(@PathVariable("id") Long id)
+    {
+        Dept dept =  this.deptService.get(id);
+        if(null == dept)
+        {
+            throw new RuntimeException("该ID："+id+"没有没有对应的信息");
+        }
+        return dept;
+    }
+
+    public Dept processHystrix_Get(@PathVariable("id") Long id)
+    {
+        return new Dept().setDeptno(id)
+                .setDname("该ID："+id+"没有没有对应的信息,null--@HystrixCommand")
+                .setDb_source("no this database in MySQL");
+    }
+
+//    public Dept get(@PathVariable("id") Long id){
+//        return deptService.get(id);
+//    }
+
+    @RequestMapping(value = "/dept/list",method = RequestMethod.GET)
+    public List<Dept> get(){
+        return deptService.list();
+    }
+}
